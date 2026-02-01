bd45fc02f60f7e57a39dd408d39cee39ed976fa0
bhuynh123 <benjaminanhhuynh101@gmail.com>
2026-02-01 13:04:02 -0800

    working on add nodes

--- main.cpp
+++ main.cpp
@@ -7,27 +7,16 @@ struct Node {
     int state[2][2]; //actualy state itself
     Node* parent;   
     string action; //backtrace to parent
-    Node* child[4]; //up, down, left, right
 
     //copy over constructor
     Node(int initialState[2][2]) {
-
         for (int i = 0; i < 2; i++) {
-
             for (int j = 0; j < 2; j++) {
-
                 state[i][j] = initialState[i][j];
-
             }
-
         }
-
         parent = nullptr;
         action = "";
-        //started hard coding all child to null but loop to better scale
-        for (int i = 0; i < 4; i++) {
-            child[i] = nullptr;
-        }
     }
 };
 
@@ -43,9 +32,9 @@ void printNode(Node* n) {
     }
 
     cout << "Action: " << n->action << endl;
-
     cout << "Parent: " << n->parent << endl;
 }
+
 
 
 
@@ -53,11 +42,32 @@ void generalSearch(int initialState[2][2]) {
     // Implement general search algorithm here
     queue<Node*> stack;
 
-    Node* startNode = new Node(initialState);
-
-    stack.push(startNode);
+    stack.push(new Node(initialState));
 
     printNode(stack.front());
+
+    do {
+
+        if(stack.empty()) {
+            cout << "No more nodes to explore." << endl;
+            break;
+        }
+
+        if (!stack.empty()) {
+            Node* currentNode = stack.front();
+            stack.pop();
+
+            // Process currentNode here (e.g., check for goal state, generate successors, etc.)
+            printNode(currentNode);
+
+            for (int i = 0; i < 3; i++) {
+
+            }
+            // For demonstration, we will not generate successors in this example.
+            // In a complete implementation, you would generate and push successor nodes onto the stack here.
+        }
+
+    } while (!stack.empty());
 
 
 };
