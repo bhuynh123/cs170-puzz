7356e57144ea28f3a4b7b51c93de03cad3b1589a
bhuynh123 <benjaminanhhuynh101@gmail.com>
2026-02-04 18:22:10 -0800

    save

--- 8puzz.cpp
+++ 8puzz.cpp
@@ -243,14 +243,16 @@ int main() {
     //up, down, left, right
     //generalSearch(initialState);
     //int testState8[3][3] = { {1, 2, 3} , {5, 0, 6} , {4, 7, 8} };
-    //int testState12[3][3] = { {1, 3, 6} , {5, 0, 2} , {4, 7, 8} };
+   // int testState8[3][3] = { {1, 3, 6} , {5, 0, 2} , {4, 7, 8} };
       //int testState15[3][3] = { {1, 6, 4} , {5, 0, 7} , {2, 3, 8} };
     //int testState[3][3] = { {4, 2, 7} , {5, 3, 8} , {6, 1, 0} };
 
-    int testState14[3][3] = { {2, 8, 3} , {1, 6, 4} , {7, 0, 5} };
+    //int testState17[3][3] = { {4, 3, 8} , {5, 0, 1} , {7, 2, 6} };
+    //int testState
+    int testState27[3][3] = { {3, 0, 7} , {8, 6, 4} , { 2, 5, 1}};
 
     auto start = std::chrono::high_resolution_clock::now();
-    generalSearch(testState14);
+    generalSearch(testState27);
     auto end = std::chrono::high_resolution_clock::now();
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     cout << "Elapsed time: " << ms << " ms (" << ms / 1000.0 << " s)" << endl;
